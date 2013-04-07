d631f45aef0609c5ecef10fb239f730f40c43d50
Nullivex <contact@nullivex.com>
2013-04-06 20:26:26 -0700

    added static config handling

---
+++ src/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$config['pdns']['db']['host'] = 'localhost';
+$config['pdns']['db']['port'] = 3306;
+$config['pdns']['db']['user'] = 'pdns';
+$config['pdns']['db']['password'] = 'pdns';
+$config['pdns']['db']['database'] = 'pdna';
